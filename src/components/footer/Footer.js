67a82a3dbf9f2836d4e50f40cf4a0a6d39aae89d
Christian Carandang <91305708+christiandc19@users.noreply.github.com>
2023-01-23 10:51:02 -0800

    styling

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import { BsFillArrowUpCircleFill } from 'react-icons/bs'
 import logo from '../../assets/nav-logo.png'
-import { FiMail, FiFacebook, FiGithub, FiInstagram, FiLinkedin, FiDribbble } from 'react-icons/fi'
+// import { FiMail, FiFacebook, FiGithub, FiInstagram, FiLinkedin, FiDribbble } from 'react-icons/fi'
 import './FooterStyles.css'
 
 import { Link as LinkRoll } from 'react-scroll'
@@ -21,25 +21,6 @@ const Footer = () => {
 
                 </div>
                 <div className="col-container">
-                    <div className="col">
-                        <h3>Navigation</h3>
-                        
-                        <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500} >
-                        <Link to='/'><p>Home</p></Link>
-                        </LinkRoll>
-
-                        <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500} >
-                        <Link to='/treatment'><p>Treatment</p></Link>
-                        </LinkRoll>
-
-                        <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500} >
-                        <Link to='/insurance'><p>Insurance</p></Link>
-                        </LinkRoll>
-
-                        <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500} >
-                        <Link to='/contact'><p>Contact</p></Link>
-                        </LinkRoll>
-                    </div>
 
                     <div className="col">
                         <h3>My Account</h3>
@@ -60,9 +41,71 @@ const Footer = () => {
                         <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500} >
                         <Link to='/'><p>Programs</p></Link>
                         </LinkRoll>
-                        
                     </div>
-                    <form>
+
+
+                    <div className="col">
+                        <h3>Navigation</h3>
+                        
+                        <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500} >
+                        <Link to='/'><p>Home</p></Link>
+                        </LinkRoll>
+
+                        <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500} >
+                        <Link to='/treatment'><p>Treatment</p></Link>
+                        </LinkRoll>
+
+                        <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500} >
+                        <Link to='/insurance'><p>Insurance</p></Link>
+                        </LinkRoll>
+
+                        <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500} >
+                        <Link to='/contact'><p>Contact</p></Link>
+                        </LinkRoll>
+                    </div>
+
+
+                    <div className="col">
+                        <h3>SUBSTANCE ABUSE</h3>
+                        <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500} >
+                        <Link to='/substance-abuse-php'><p>Partial Hospitalization Program</p></Link>
+                        </LinkRoll>
+
+                        <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500} >
+                        <Link to='/substance-abuse-iop'><p>Intensive Outpatient Program</p></Link>
+                        </LinkRoll>
+
+                        {/* <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500} >
+                        <Link to='/testimonials'><p>Testimonials</p></Link>
+                        </LinkRoll> */}
+
+                        <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500} >
+                        <Link to='/substance-abuse-op'><p>Outpatient Program</p></Link>
+                        </LinkRoll>
+                    </div>
+
+
+                    <div className="col">
+                        <h3>MENTAL HEALTH</h3>
+                        <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500} >
+                        <Link to='/mental-health-php'><p>Partial Hospitalization Program</p></Link>
+                        </LinkRoll>
+
+                        <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500} >
+                        <Link to='/mental-health-iop'><p>Intensive Outpatient Program</p></Link>
+                        </LinkRoll>
+
+                        {/* <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500} >
+                        <Link to='/testimonials'><p>Testimonials</p></Link>
+                        </LinkRoll> */}
+
+                        <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500} >
+                        <Link to='/mental-health-op'><p>Outpatient Program</p></Link>
+                        </LinkRoll>
+                    </div>
+
+
+                    {/* <form>
                         <h3>Join Our Team</h3>
                         <input type="email" placeholder='Enter your email' />
                         <FiMail className='mail-icon' />
@@ -73,7 +116,7 @@ const Footer = () => {
                             <FiDribbble className='social-icon' />
                             <FiGithub className='social-icon' />
                         </div>
-                    </form>
+                    </form> */}
                 </div>
             </div>
         </div>
