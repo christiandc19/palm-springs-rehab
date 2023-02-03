0a9863a9cefe59271cc0cecafe1d38b61aaa7f9d
Christian Carandang <91305708+christiandc19@users.noreply.github.com>
2023-02-03 10:35:59 -0800

    styling

--- src/components/about/AboutSection.js
+++ src/components/about/AboutSection.js
@@ -6,7 +6,7 @@ const AboutSection = () => {
     return (
         <>
         <div className='about-section container'>
-        <h1>Who We Are</h1>
+        <h1>WHO WE ARE</h1>
         <p>Palm Springs Rehab Treatment Center has refined and well educated staff that is committed to providing the best possible level of care to our clients. We are there for you, through every stage of recovery. No matter where you’re located, you can reach out and get in touch with someone from our staff today</p>
         </div>
         </>
