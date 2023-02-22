e90be70d0d5462fc05de5dcb01ebf5ec7bb825b5
Christian Carandang <91305708+christiandc19@users.noreply.github.com>
2023-02-22 15:35:40 -0800

    styling

--- src/components/servicesCards/Cards.js
+++ src/components/servicesCards/Cards.js
@@ -18,7 +18,7 @@ const Cards = () => {
 
 <div className="cards-header container">
     <h1>WE OFFER VARIOUS SERVICES </h1>
-    <p>At Palm Springs Rehab Treatment Center, clients receive individual therapy alongside a range of additional treatments, including relapse prevention, trauma therapy, mindfulness meditation, transportation, yoga, and more. We offer support that clients need to allow them to gradually go back to treatment. </p>
+    <p>At<span> Palm Springs Rehab Treatment Center</span>, clients receive individual therapy alongside a range of additional treatments, including relapse prevention, trauma therapy, mindfulness meditation, transportation, yoga, and more. We offer support that clients need to allow them to gradually go back to treatment. </p>
 </div>
 
 <div class="card-wrap">
