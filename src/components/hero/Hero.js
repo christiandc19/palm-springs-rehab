27bda403a526f3fe0d22433d728ed02d8cf53af5
github_christiandc19 <christiandc19@hotmail.com>
2023-10-28 08:36:44 -0700

    styling

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,45 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import Fade from 'react-reveal/Fade';
-import { Link as LinkRoll } from "react-scroll";
-
-
-
-
-
-import Slide1 from '../../assets/slider1-min.webp'
-import Slide2 from '../../assets/slider2-min.webp'
-import Slide3 from '../../assets/slider3-min.webp'
-
 import './Hero.css'
+
+import BgVideo from '../../assets/Bg-Video2.mp4'
+
 
 const Hero = () => {
     return (
-        <>
-<div className="slider">
-	<ul>
-		<li><img src={Slide1} alt="Conselling Session with a patient" loading="lazy" /></li>
-		<li><img src={Slide2} alt="Conselling Session with a patient" loading="lazy" /></li>
-		<li><img src={Slide3} alt="Conselling Session with a patient" loading="lazy" /></li>
-	</ul>
+<>        
 
-    <div className='hero-caption'> 
-    <Fade left>
-        <p>A Journey to Healing and Renewed Well-being</p>
-        <h1>PALM SPRINGS REHAB</h1>
-    </Fade>
-        <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500}>
-        <Link to='/contact'>
-        <button>
-            CALL US
-        </button>
-        </Link>
-        </LinkRoll>
-    </div>
+        <div className='hero'>
+            <video src={BgVideo} autoPlay muted loop playsInline className='video-bg' />
+            <div className="hero-container">
 
-</div>
+                <div className="content">
+                        <h2>A journey to Healing and Renewed Well-being</h2>
+                        <h1>PALM SPRINGS REHAB</h1>
+                    <Fade left>
+                        <h3>Addiction and Mental Health Facility</h3>
+                    </Fade>
+                        <div className='hero-btn'>
+                        <Link to="/contact">
+                        <button>CALL US</button>
+                    </Link>
+                    </div>
 
-        </>
+                </div>
+            </div>
+        </div>
+
+
+</>
+
     )
 }
 
